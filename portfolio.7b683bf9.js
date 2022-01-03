ea8caf0aeb47ca60051a5f963ffe3c5a2ac07ea8
romanbelei <45937404+romanbelei@users.noreply.github.com>
2022-01-03 21:03:03 +0000

    Deploying to gh-pages from @ romanbelei/mySiteForComands@89f59addeb599e3105df4c07b69946efaa0750d2 🚀

--- portfolio.7b683bf9.js
+++ portfolio.7b683bf9.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"pzHV":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["pzHV"], null)
-//# sourceMappingURL=/mySiteForComands/portfolio.b184c858.js.map
+//# sourceMappingURL=/mySiteForComands/portfolio.7b683bf9.js.map
--- src.9bef51d4.js
+++ src.9bef51d4.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/mySiteForComands/src.ddd1e8d2.js.map
+//# sourceMappingURL=/mySiteForComands/src.9bef51d4.js.map
