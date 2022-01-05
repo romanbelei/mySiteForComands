c1cdbc45419bb06ddc29742e0ea720cc75544f48
romanbelei <45937404+romanbelei@users.noreply.github.com>
2022-01-05 08:03:23 +0000

    Deploying to gh-pages from @ romanbelei/mySiteForComands@f990bcafc844c0eed1e98674ad15c9b5984f09ea 🚀

--- portfolio.a105b965.js
+++ portfolio.a105b965.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"pzHV":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["pzHV"], null)
-//# sourceMappingURL=/mySiteForComands/portfolio.23ae9377.js.map
+//# sourceMappingURL=/mySiteForComands/portfolio.a105b965.js.map
